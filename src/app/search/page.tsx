4a24bd5601e92dd5afb0695385866d5a5f0678f3
glennhkm <glenn@mhs.usk.ac.id>
2024-11-28 00:41:32 +0700

    fix: remove dynamic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,5 @@
 import { Suspense } from "react";
-import dynamic from 'next/dynamic';
-
-// Dynamic import with SSR disabled
-const SearchClient = dynamic(
-  () => import('./searchClient'),
-  { 
-    ssr: false,
-    loading: () => <p>Loading...</p>
-  }
-);
+import SearchClient from "./searchClient";
 
 export default function ResultPage() {
   return (
